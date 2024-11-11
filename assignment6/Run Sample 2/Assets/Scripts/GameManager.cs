6173fdd44714c8883abdbd745e7e0d5a93db0f3f
finimage <finimage@korea.ac.kr>
2024-11-11 21:49:34 +0900

    edit GameManager.cs

--- assignment6/Run Sample 2/Assets/Scripts/GameManager.cs
+++ assignment6/Run Sample 2/Assets/Scripts/GameManager.cs
@@ -36,7 +36,7 @@ public class GameManager : MonoBehaviour
     {
         // CamObj는 Character의 x, y position을 따라간다.
         // ---------- TODO ---------- 
-        if (CamObj && Character)
+        if (CamObj && Character) // CamObj와 Character가 존재하면
         {
             CamObj.transform.position = new Vector3(Character.transform.position.x, Character.transform.position.y, CamObj.transform.position.z);
         }
@@ -47,8 +47,9 @@ public class GameManager : MonoBehaviour
     {
         // Character는 초당 CharacterSpeed의 속도로 우측으로 움직인다.
         // ---------- TODO ---------- 
-        if (Character)
+        if (Character) // Character가 존재하면
         {
+            //Time.deltaTime 안 곱하면 초당 이동이 아님
             Character.transform.position += new Vector3(CharacterSpeed, 0, 0) * Time.deltaTime;
         }
         // -------------------- 
@@ -58,7 +59,7 @@ public class GameManager : MonoBehaviour
     {
         // Character를 삭제하고, "Game Over!"라는 메시지를 3초간 띄우고, RestartButton을 활성화한다.
         // ---------- TODO ---------- 
-        if (Character)
+        if (Character) // Character가 존재하면
         {
             Destroy(Character);
             MyUIManager.DisplayMessage("Game Over!", 3);//parameter: message, duration
